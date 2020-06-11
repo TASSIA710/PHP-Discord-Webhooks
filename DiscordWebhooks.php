7034b85b84ca131eb699965c2e3ba05c62547f18
Tassilo <tassia710@gmail.com>
2020-06-11 20:16:51 +0200

    Finished all functionalities
    Documentation is too come.

--- DiscordWebhooks.php
+++ DiscordWebhooks.php
@@ -155,7 +155,19 @@ class DiscordWebhook {
 
 
     /* Embeds */
-    // TODO
+    public function getEmbeds() {
+        return $this->embeds;
+    }
+
+    public function clearEmbeds() {
+        $this->embeds = [];
+        return $this;
+    }
+
+    public function addEmbed($embed) {
+        $this->embeds[count($this->embeds)] = $embed;
+        return $this;
+    }
     /* Embeds */
 
 
@@ -167,7 +179,14 @@ class DiscordWebhook {
         if ($this->username != null) $obj['username'] = $this->username;
         if ($this->avatar_url != null) $obj['avatar_url'] = $this->avatar_url;
         if ($this->tts != false) $obj['tts'] = $this->tts;
-        // TODO: embeds
+
+        if (count($this->embeds) != 0) {
+            $obj['embeds'] = [];
+            foreach ($this->embeds as $index => $embed) {
+                $obj['embeds'][$index] = $embed->toDiscordObject();
+            }
+        }
+
         return $obj;
     }
     /* Internal */
@@ -185,6 +204,9 @@ class DiscordWebhook {
         $json = $this->toDiscordObject();
         $json = json_encode($json);
 
+        echo $json;
+        echo "\n";
+
         $ch = curl_init($url);
         curl_setopt($ch, CURLOPT_POST, true);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
@@ -201,6 +223,7 @@ class DiscordWebhook {
             curl_close($ch);
             return false;
         } else {
+            echo $result;
             $response_code = curl_getinfo($ch, CURLINFO_RESPONSE_CODE);
             curl_close($ch);
             return $response_code;
@@ -212,9 +235,9 @@ class DiscordWebhook {
 
 
 /**
- * A WebhookEmbed represents an embed within a webhook payload.
+ * A DiscordWebhookEmbed represents an embed within a webhook payload.
  */
-class WebhookEmbed {
+class DiscordWebhookEmbed {
     private $title, $description, $url, $timestamp, $color, $fields;
     private $footer_text, $footer_icon_url;
     private $image_url;
@@ -222,6 +245,9 @@ class WebhookEmbed {
     private $author_name, $author_url, $author_icon_url;
 
     /* Constructor */
+    /**
+     * Creates a new DiscordWebhookEmbed.
+     */
     public function __construct() {
         $this->title = null;
         $this->description = null;
@@ -239,6 +265,253 @@ class WebhookEmbed {
     }
     /* Constructor */
 
+
+
+    /* Title */
+    public function getTitle() {
+        return $this->title;
+    }
+
+    public function setTitle($title = null) {
+        $this->title = $title;
+        return $this;
+    }
+    /* Title */
+
+
+
+    /* Description */
+    public function getDescription() {
+        return $this->description;
+    }
+
+    public function setDescription($description = null) {
+        $this->description = $description;
+        return $this;
+    }
+    /* Description */
+
+
+
+    /* URL */
+    public function getURL() {
+        return $this->url;
+    }
+
+    public function setURL($url = null) {
+        $this->url = $url;
+        return $this;
+    }
+    /* URL */
+
+
+
+    /* Timestamp */
+    public function getTimestamp() {
+        return $this->timestamp;
+    }
+
+    public function setTimestamp($timestamp = null) {
+        if (is_numeric($timestamp)) {
+            $this->timestamp = date('c', $timestamp);
+        } else {
+            $this->timestamp = $timestamp;
+        }
+        return $this;
+    }
+    /* Timestamp */
+
+
+
+    /* Color */
+    public function getColor() {
+        if ($this->color == null) return null;
+        return [
+            ($this->color & 0xff0000) >> 16,
+            ($this->color & 0x00ff00) >> 8,
+            ($this->color & 0x0000ff)
+        ];
+    }
+
+    public function getColorInt() {
+        return $this->color;
+    }
+
+    public function setColor($red = 255, $green = 255, $blue = 255) {
+        $int = $red;
+        $int <<= 8;
+        $int |= $green;
+        $int <<= 8;
+        $int |= $blue;
+        return $this->setColorInt($int);
+    }
+
+    public function setColorInt($int) {
+        $this->color = $int;
+        return $this;
+    }
+
+    public function setColorHex($hex) {
+        return $this->setColorInt(hexdec($hex));
+    }
+    /* Color */
+
+
+
+    /* Fields */
+    public function getFields() {
+        return $this->fields;
+    }
+
+    public function clearFields() {
+        $this->fields = [];
+        return $this;
+    }
+
+    public function addField($field) {
+        $this->fields[count($this->fields)] = $field;
+        return $this;
+    }
+    /* Fields */
+
+
+
+    /* Footer */
+    public function getFooterText() {
+        return $this->footer_text;
+    }
+
+    public function getFooterAvatar() {
+        return $this->footer_icon_url;
+    }
+
+    public function setFooter($text = null, $avatar = null) {
+        $this->footer_text = $text;
+        $this->footer_icon_url = $avatar;
+        return $this;
+    }
+
+    public function setFooterText($text = null) {
+        $this->footer_text = $text;
+        return $this;
+    }
+
+    public function setFooterAvatar($url = null) {
+        $this->footer_icon_url = $url;
+        return $this;
+    }
+    /* Footer */
+
+
+
+    /* Image */
+    public function getImage() {
+        return $this->image_url;
+    }
+
+    public function setImage($url = null) {
+        $this->image_url = $url;
+        return $this;
+    }
+    /* Image */
+
+
+
+    /* Thumbnail */
+    public function getThumbnail() {
+        return $this->thumbnail_url;
+    }
+
+    public function setThumbnail($url = null) {
+        $this->thumbnail_url = $url;
+        return $this;
+    }
+    /* Thumbnail */
+
+
+
+    /* Author */
+    public function getAuthorName() {
+        return $this->author_name;
+    }
+
+    public function getAuthorURL() {
+        return $this->author_url;
+    }
+
+    public function getAuthorAvatar() {
+        return $this->author_icon_url;
+    }
+
+    public function setAuthor($name = null, $url = null, $avatar = null) {
+        $this->author_name = $name;
+        $this->author_url = $url;
+        $this->author_icon_url = $avatar;
+        return $this;
+    }
+
+    public function setAuthorName($name = null) {
+        $this->author_name = $name;
+        return $this;
+    }
+
+    public function setAuthorURL($url = null) {
+        $this->author_url = $url;
+        return $this;
+    }
+
+    public function setAuthorAvatar($url = null) {
+        $this->author_icon_url = $url;
+        return $this;
+    }
+    /* Author */
+
+
+
+    /* Internal */
+    public function toDiscordObject() {
+        $obj = [];
+        if ($this->title != null) $obj['title'] = $this->title;
+        if ($this->description != null) $obj['description'] = $this->description;
+        if ($this->url != null) $obj['url'] = $this->url;
+        if ($this->timestamp != null) $obj['timestamp'] = $this->timestamp;
+        if ($this->color != null) $obj['color'] = $this->color;
+
+        if ($this->footer_text != null || $this->footer_icon_url != null) {
+            $obj['footer'] = [];
+            if ($this->footer_text != null) $obj['footer']['text'] = $this->footer_text;
+            if ($this->footer_icon_url != null) $obj['footer']['icon_url'] = $this->footer_icon_url;
+        }
+
+        if ($this->image_url != null) {
+            $obj['image'] = [
+                'url' => $this->image_url
+            ];
+        }
+
+        if ($this->thumbnail_url != null) {
+            $obj['thumbnail'] = [
+                'url' => $this->thumbnail_url
+            ];
+        }
+
+        if ($this->author_name != null || $this->author_url != null || $this->author_icon_url != null) {
+            $obj['author'] = [];
+            if ($this->author_name != null) $obj['author']['name'] = $this->author_name;
+            if ($this->author_url != null) $obj['author']['url'] = $this->author_url;
+            if ($this->author_icon_url != null) $obj['author']['icon_url'] = $this->author_icon_url;
+        }
+
+        if (count($this->fields) != 0) {
+            $obj['fields'] = [];
+            foreach ($this->fields as $index => $field) {
+                $obj['fields'][$index] = $field->toDiscordObject();
+            }
+        }
+        return $obj;
+    }
+    /* Internal */
+
 }
 
 
@@ -246,14 +519,14 @@ class WebhookEmbed {
 
 
 /**
- * A WebhookEmbedField represents a field within an embed.
+ * A DiscordWebhookEmbedField represents a field within an embed.
  */
-class WebhookEmbedField {
+class DiscordWebhookEmbedField {
     private $name, $value, $inline;
 
     /* Constructor */
     /**
-     * Creates a new WebhookEmbedField.
+     * Creates a new DiscordWebhookEmbedField.
      * @param string $name the field's name
      * @param string $value the field's value
      * @param bool $inline whether this field is an inline field
@@ -277,7 +550,7 @@ class WebhookEmbedField {
 
     /**
      * @param string $name
-     * @return WebhookEmbedField this
+     * @return DiscordWebhookEmbedField this
      */
     public function setName($name = '') {
         $this->name = $name;
@@ -297,7 +570,7 @@ class WebhookEmbedField {
 
     /**
      * @param string $value
-     * @return WebhookEmbedField this
+     * @return DiscordWebhookEmbedField this
      */
     public function setValue($value = '') {
         $this->value = $value;
@@ -317,7 +590,7 @@ class WebhookEmbedField {
 
     /**
      * @param bool $inline
-     * @return WebhookEmbedField this
+     * @return DiscordWebhookEmbedField this
      */
     public function setInline($inline = false) {
         $this->inline = $inline;
@@ -328,7 +601,7 @@ class WebhookEmbedField {
 
 
     /* Internal */
-    protected function toDiscordObject() {
+    public function toDiscordObject() {
         return [
             'name' => $this->name,
             'value' => $this->value,
